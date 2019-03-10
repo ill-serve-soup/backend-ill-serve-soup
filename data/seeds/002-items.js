df4583560e592b26c8cfb77803c97cbdb7129aca
Zooheck <davidscottflack@gmail.com>
2019-03-09 19:55:34 -0600

    Update userIds for spencer

--- data/seeds/002-items.js
+++ data/seeds/002-items.js
@@ -6,92 +6,92 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex("items").insert([
         {
-          userId: 2,
+          userId: 11,
           name: "carrots",
           qty: 20,
           units: "lbs",
           categoryId: 2
         },
         {
-          userId: 2,
+          userId: 11,
           name: "chicken breast",
           qty: 15,
           units: "lbs",
           categoryId: 3
         },
         {
-          userId: 2,
+          userId: 11,
           name: "ground beef",
           qty: 10,
           units: "lbs",
           categoryId: 3
         },
         {
-          userId: 2,
+          userId: 11,
           name: "tilapia",
           qty: 16,
           units: "lbs",
           categoryId: 3
         },
         {
-          userId: 2,
+          userId: 11,
           name: "lettuce",
           qty: 5,
           units: "lbs",
           categoryId: 2
         },
         {
-          userId: 2,
+          userId: 11,
           name: "bananas",
           qty: 50,
           categoryId: 2
         },
         {
-          userId: 2,
+          userId: 11,
           name: "flour",
           qty: 10,
           units: "lbs",
           categoryId: 4
         },
         {
-          userId: 2,
+          userId: 11,
           name: "salt",
           qty: 2,
           units: "lbs",
           categoryId: 5
         },
         {
-          userId: 2,
+          userId: 11,
           name: "refrigerator",
           qty: 2,
           categoryId: 8
         },
         {
-          userId: 2,
+          userId: 11,
           name: "forks",
           qty: 150,
           categoryId: 6
         },
         {
-          userId: 2,
+          userId: 11,
           name: "spoons",
           qty: 150,
           categoryId: 6
         },
         {
-          userId: 2,
+          userId: 11,
           name: "knives",
           qty: 100,
           categoryId: 6
         },
         {
-          userId: 2,
+          userId: 11,
           name: "bowls",
           qty: 200,
           categoryId: 7
         },
         {
-          userId: 2,
+          userId: 11,
           name: "plates",
           qty: 200,
           categoryId: 7
