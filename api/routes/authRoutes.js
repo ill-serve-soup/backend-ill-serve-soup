74d0d0db8bb74b8e17d68008246959982c9d0180
Zooheck <davidscottflack@gmail.com>
2019-03-11 10:49:32 -0500

    Fix seed bug

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -33,13 +33,11 @@ router.post("/login", async (req, res) => {
   let { email, password } = req.body;
   try {
     const user = await UserFuncs.findBy({ email });
-    console.log(user);
-    console.log(user.password);
-    console.log(bcrypt.compareSync(password, user.password));
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = generateToken(user);
       res.status(200).json({
         message: `Welcome, ${user.firstName}!`,
+        userId: user.userId,
         token
       });
     } else {
