81561d047851a8b80aacb7bada4998aa9d84904b
Zooheck <davidscottflack@gmail.com>
2019-03-10 23:19:51 -0500

    Add description to seed items

--- data/seeds/002-items.js
+++ data/seeds/002-items.js
@@ -10,91 +10,105 @@ exports.seed = function(knex, Promise) {
           name: "carrots",
           qty: 20,
           units: "lbs",
-          categoryId: 2
+          categoryId: 2,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "chicken breast",
           qty: 15,
           units: "lbs",
-          categoryId: 3
+          categoryId: 3,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "ground beef",
           qty: 10,
           units: "lbs",
-          categoryId: 3
+          categoryId: 3,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "tilapia",
           qty: 16,
           units: "lbs",
-          categoryId: 3
+          categoryId: 3,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "lettuce",
           qty: 5,
           units: "lbs",
-          categoryId: 2
+          categoryId: 2,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "bananas",
           qty: 50,
-          categoryId: 2
+          categoryId: 2,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "flour",
           qty: 10,
           units: "lbs",
-          categoryId: 4
+          categoryId: 4,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "salt",
           qty: 2,
           units: "lbs",
-          categoryId: 5
+          categoryId: 5,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "refrigerator",
           qty: 2,
-          categoryId: 8
+          categoryId: 8,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "forks",
           qty: 150,
-          categoryId: 6
+          categoryId: 6,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "spoons",
           qty: 150,
-          categoryId: 6
+          categoryId: 6,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "knives",
           qty: 100,
-          categoryId: 6
+          categoryId: 6,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "bowls",
           qty: 200,
-          categoryId: 7
+          categoryId: 7,
+          description: "Lorem ipsum dolor sit amet"
         },
         {
           userId: 11,
           name: "plates",
           qty: 200,
-          categoryId: 7
+          categoryId: 7,
+          description: "Lorem ipsum dolor sit amet"
         }
       ]);
     });
