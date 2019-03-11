395e54de5d0061c092e6eb445c22c20a5b2d6a21
Zooheck <davidscottflack@gmail.com>
2019-03-10 22:34:49 -0500

    Prepare for heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,6 @@
-// Update with your config settings.
+require("dotenv").config();
+const pg = require("pg");
+pg.defaults.ssl = true;
 
 module.exports = {
   development: {
@@ -15,35 +17,19 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 };
