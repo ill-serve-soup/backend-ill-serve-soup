34de4a87a21a40fc8a654e1f1f2156c9c0550ad2
Zooheck <davidscottflack@gmail.com>
2019-03-14 12:43:51 -0500

    Add tests that server is working

--- api/server.spec.js
+++ api/server.spec.js
@@ -7,3 +7,21 @@ describe("server.js", () => {
     expect(process.env.DB_ENV).toBe("testing");
   });
 });
+
+describe("get /", () => {
+  it("should return http 200", async () => {
+    const res = await request(server).get("/");
+
+    expect(res.status).toBe(200);
+  });
+  it("should return JSON", async () => {
+    const res = await request(server).get("/");
+
+    expect(res.type).toBe("application/json");
+  });
+  it("should return server is working", async () => {
+    const res = await request(server).get("/");
+
+    expect(res.body).toEqual({ message: "Server is working." });
+  });
+});
