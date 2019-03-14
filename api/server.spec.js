263680e3c502e2d57637f924fcdb2158d8753106
Zooheck <davidscottflack@gmail.com>
2019-03-14 13:56:41 -0500

    Test register routes

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 
 const server = require("./server.js");
-
+const db = require("../dbconfig.js");
 describe("server.js", () => {
   it("should set testing environment", () => {
     expect(process.env.DB_ENV).toBe("testing");
@@ -23,5 +23,26 @@ describe("get /", () => {
     const res = await request(server).get("/");
 
     expect(res.body).toEqual({ message: "Server is working." });
+  });
+});
+const newUser = {
+  firstName: "admin",
+  lastName: "admin",
+  email: "admin",
+  password: "admin"
+};
+const badUser = {
+  email: "admin2",
+  password: "admin2"
+};
+describe("/auth", () => {
+  afterEach(async () => {
+    await db("users").truncate();
+  });
+  it("/register should return 201 with valid params", async () => {
+    const res = await request(server)
+      .post("/auth/register")
+      .send(newUser);
+    expect(res.status).toBe(201);
   });
 });
